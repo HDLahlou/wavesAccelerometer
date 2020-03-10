2b4d28aad9a01a73e710357925c5a6d245431637
HDlahlou <lahlou.hakim1@gmail.com>
2020-03-10 12:02:54 -0500

    updated for gyroscope collection and readme documentation

--- App.js
+++ App.js
@@ -1,40 +1,42 @@
 import React, { Component } from 'react';
 import { Text, Picker, View, StyleSheet, Dimensions, Button } from 'react-native';
-import { Constants, Accelerometer } from 'expo-sensors';
+import { Constants, Accelerometer, Gyroscope } from 'expo-sensors';
 import * as MediaLibrary from 'expo-media-library';
 import * as FileSystem from 'expo-file-system';
 import * as Permissions from 'expo-permissions';
 import { Link } from "./Linking"
 export default class App extends Component<{}> {
   state = {
-    // Of type accelermoterData, will be a json of arrays
-    accelerometerDataTest: [],
-
-    currentCollectIndex: 0,
-    dropDownVal: [0,1,2],
-    accelerometerDataHolder: [],
-    initialTime: Date.now(),
+    // State Variables for Accelerometer Data
+    accelerometerDataTest: [[],[],[],[],[],[]],
     accelerometerData: { time: 0, x: 0, y: 0, z: 0 },
+    accelerometerDataHolder: [],
+    // State Variables for Gyroscope Data
+    gyroscopeDataTest: [[],[],[],[],[],[]],
+    gyroscopeData: { time: 0, x: 0, y: 0, z: 0 },
+    gyroscopeDataHolder: [],
+    // General State Variables
+    currentCollectIndex: 0,
+    initialTime: Date.now(),
     recordData: false,
   };
 
   componentWillUnmount() {
     this._unsubscribeFromAccelerometer();
+    this._unsubscribeFromGyroscope();
   }
 
   componentDidMount() {
     this._subscribeToAccelerometer();
+    this._subscribeToGyroscope();
   }
 
   componentWillMount() {
-    const { width, height } = Dimensions.get('window');
-    this.screenWidth = width;
-    this.screenHeight = height;
-    this.boxWidth = this.screenWidth / 10.0;
-    let array = [[],[],[]]
-    this.setState( {accelerometerDataTest: array})
+    // let array = [[],[],[]]
+    // this.setState( {accelerometerDataTest: array, gyroscopeDataTest: array})
   }
 
+/**********ACCELEROMETER LISTENER*********/
   _subscribeToAccelerometer = () => {
     this._accelerometerSubscription = Accelerometer.addListener(
       accelData => {
@@ -58,23 +60,82 @@ export default class App extends Component<{}> {
     );
   };
 
+  _unsubscribeFromAccelerometer = () => {
+    // this._accelerometerSubscription && this._acceleroMeterSubscription.remove();
+    // this._accelerometerSubscription = null;
+  };
+/****************************************/
+
+/**********GYROSCOPE LISTENER*********/
+_subscribeToGyroscope = () => {
+  this._gyroSubscription = Gyroscope.addListener(gyroscopeData => {
+    let currentTime = Date.now();
+    let intermediateValue = { time: (currentTime-this.state.initialTime), x: gyroscopeData.x, y: gyroscopeData.y, z: gyroscopeData.z}
+    if(this.state.recordData){
+      let newArray = [... this.state.gyroscopeDataHolder, intermediateValue];
+      this.setState((prevState, props) => {
+        return {
+          gyroscopeDataHolder: newArray
+        }
+      })
+    }
+    this.setState((prevState, props) => {
+      return {
+        gyroscopeData: intermediateValue,
+      }
+    })
+  });
+};
+
+_unsubscribeFromGyroscope = () => {
+  this._gyroSubscription && this._gyroSubscription.remove();
+  this._gyroSubscription = null;
+};
+/****************************************/
+
+// Saves the current Accel Data into a text document
   pressButtonToLog = () => {
     // console.log(this.state.accelerometerDataTest);
     console.log(" Logging into text doc");
     this.saveFile()
   }
 
+  // Method to save data to txt file
   saveFile = async () => {
     const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
     if (status === "granted") {
-      let stringToPrint = JSON.stringify(this.state.accelerometerDataTest);
-      let fileUri = FileSystem.documentDirectory + "WAVES_TEST_ACCEL_DATA.txt";
+      let accelStringToPrint = JSON.stringify(this.state.accelerometerDataTest);
+      let gyroStringToPrint = JSON.stringify(this.state.gyroscopeDataTest);
+      let stringToPrint = `Accelerometer:\n${accelStringToPrint}\nGyroscope:\n${gyroStringToPrint}`
+      let fileUri = FileSystem.documentDirectory + "WAVES_TEST_ACCEL_GYRO_DATA.txt";
       await FileSystem.writeAsStringAsync(fileUri, stringToPrint, { encoding: FileSystem.EncodingType.UTF8 });
       const asset = await MediaLibrary.createAssetAsync(fileUri)
       await MediaLibrary.createAlbumAsync("Download", asset, false)
   }
   }
 
+// Removes most recent value in array
+removeLastEntry = () => {
+  let {recordData, currentCollectIndex, accelerometerDataTest, gyroscopeDataTest} = this.state
+  let arrayLength = accelerometerDataTest[currentCollectIndex].length;
+  if(!recordData && arrayLength){
+    let arrayAccel = accelerometerDataTest;
+    arrayAccel[currentCollectIndex].pop();
+    let arrayGyro = gyroscopeDataTest;
+    arrayGyro[currentCollectIndex].pop();
+    this.setState((prevState, props) => {
+      return {
+        accelerometerDataTest: arrayAccel,
+        accelerometerDataHolder: [],
+        gyroscopeDataTest: arrayGyro,
+        gyroscopeDataHolder: [],
+      }
+    })
+    this.testCurrentDataTest()
+  }
+}
+
+// Starts recording data and initializes variables
   pressButtonToStart = () => {
     let {recordData, currentCollectIndex, accelerometerDataTest} = this.state
 
@@ -86,47 +147,51 @@ export default class App extends Component<{}> {
       return {
         initialTime: currentTime,
         recordData: !prevState.recordData,
-        accelerometerDataHolder: []
+        accelerometerDataHolder: [],
+        gyroscopeDataHolder: []
       }
     })
   }
 
+// Calls func to add data to array
   pressButtonToIncrement = () => {
     this.pushDataToArray()
   }
 
+// Func to add current recording to array
   pushDataToArray = () =>{
-    let {accelerometerDataHolder, currentCollectIndex, accelerometerDataTest} = this.state
+    let {accelerometerDataHolder, currentCollectIndex, accelerometerDataTest, gyroscopeDataHolder, gyroscopeDataTest} = this.state
     let currentTime = Date.now();
-    let array = accelerometerDataTest;
-    array[currentCollectIndex].push(accelerometerDataHolder)
+    let arrayAccel = accelerometerDataTest;
+    arrayAccel[currentCollectIndex].push(accelerometerDataHolder)
+    let arrayGyro = gyroscopeDataTest;
+    arrayGyro[currentCollectIndex].push(gyroscopeDataHolder)
     this.setState((prevState, props) => {
       return {
         initialTime: currentTime,
-        // Test this
-        accelerometerDataTest: array,
+        accelerometerDataTest: arrayAccel,
         accelerometerDataHolder: [],
+        gyroscopeDataTest: arrayGyro,
+        gyroscopeDataHolder: [],
       }
     })
 
     this.testCurrentDataTest()
   }
-
+// TEST: Prints current size of data holders
   testCurrentDataTest = () => {
-    let {accelerometerDataHolder, currentCollectIndex, accelerometerDataTest} = this.state
+    let {accelerometerDataHolder, currentCollectIndex, accelerometerDataTest, gyroscopeDataTest} = this.state
     console.log("==========TEST=============")
     accelerometerDataTest.forEach(dataArray =>{
-      console.log("Arrray Length:" + dataArray.length);
+      console.log("Accel Arrray Length:" + dataArray.length);
+    })
+    gyroscopeDataTest.forEach(dataArray =>{
+      console.log("Gyro Arrray Length:" + dataArray.length);
     })
     console.log("===========================")
   }
-// //accelermoterDataTest[prevstate.currentCollectIndex]: accelermoterDataTest[0].push(prevstate.accelerometerDataHolder)
 
-_unsubscribeFromAccelerometer = () => {
-  // this._accelerometerSubscription && this._acceleroMeterSubscription.remove();
-  // this._accelerometerSubscription = null;
-};
-
+// RENDER
 render() {
   let {currentCollectIndex, accelerometerDataTest} = this.state
   let startButton = (this.state.recordData)? "Stop": "Start"
@@ -137,26 +202,40 @@ render() {
     <View style={styles.container}>
       <View style={styles.textContainer}>
         <Text style={styles.paragraph}>
+          {takeNum}
+        </Text>
+        <Text style={styles.paragraph}>
+          {'Accel: '}
           x = {this.state.accelerometerData.x.toFixed(2)}
           {', '}y = {this.state.accelerometerData.y.toFixed(2)}
           {', '}z = {this.state.accelerometerData.z.toFixed(2)}
           {', '}time = {timeDisp}
-
+        </Text>
+        <Text style={styles.paragraph}>
+          {'Gyro: '}
+          x = {this.state.gyroscopeData.x.toFixed(2)}
+          {', '}y = {this.state.gyroscopeData.y.toFixed(2)}
+          {', '}z = {this.state.gyroscopeData.z.toFixed(2)}
+          {', '}time = {timeDisp}
         </Text>
       </View>
       <View style={styles.buttonContainer}>
         <Button style={styles.buttons} onPress={this.pressButtonToLog} title="LOG RESULTS"></Button>
         <Button style={styles.buttons} onPress={this.pressButtonToStart} title={startButton}></Button>
-        <Button style={styles.buttons} onPress={this.pressButtonToIncrement} title={takeNum}></Button>
+        {/* <Button style={styles.buttons} onPress={this.pressButtonToIncrement} title={takeNum}></Button> */}
         <Button style={styles.buttons} onPress={this.testCurrentDataTest} title="Test"></Button>
+        <Button style={styles.buttons} onPress={this.removeLastEntry} title="Drop Last"></Button>
         <Picker
           selectedValue={"G" + this.state.currentCollectIndex}
           onValueChange={currentCollectIndex => this.setState({ currentCollectIndex })}
           style={{ width: 50 }}
           mode="dropdown">
-          <Picker.Item label="G1" value="0" />
-          <Picker.Item label="G2" value="1" />
-          <Picker.Item label="G3" value="2" />
+          <Picker.Item label="G0" value="0" />
+          <Picker.Item label="G1" value="1" />
+          <Picker.Item label="G2" value="2" />
+          <Picker.Item label="G3" value="3" />
+          <Picker.Item label="G4" value="4" />
+          <Picker.Item label="G5" value="5" />
         </Picker>
       </View>
       <View style={styles.buttonContainer}>
@@ -167,6 +246,8 @@ render() {
 }
 }
 
+
+// STYLING
 const styles = StyleSheet.create({
 container: {
   flex: 1,
--- README.txt
+++ README.txt
@@ -15,3 +15,20 @@ Tell Insight:
 - 3 gestures, but arent recognized by daily actions
 - train user data individually
 - look at a systems perspective, if you are the user, how do you accept this as valiable
+
+HOW TO:
+
+General Documentation:
+https://reactnative.dev/docs/getting-started
+
+PreReq: 
+Install npm
+https://www.npmjs.com/get-npm
+Install Nodejs
+https://nodejs.org/en/download/
+
+General Reference for expo:
+https://expo.io/learn
+
+To Run Server:
+$npm start
